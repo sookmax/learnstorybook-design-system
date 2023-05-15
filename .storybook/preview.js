aee801e5d58dad7579ab3d3a07bf5fa3ab6826dc
sook <sukkyu.chung@gmail.com>
2023-05-15 17:26:53 +0900

    continue following the tutorial with a workaround for openssl issue

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,25 @@
-/** @type { import('@storybook/react').Preview } */
-const preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-  },
-};
+import React from "react";
 
-export default preview;
+import { GlobalStyle } from "../src/shared/global";
+
+/*
+ * Global decorator to apply the styles to all stories
+ * Read more about them at:
+ * https://storybook.js.org/docs/react/writing-stories/decorators#global-decorators
+ */
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
+
+/*
+ * Read more about global parameters at:
+ * https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
+ */
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+};
